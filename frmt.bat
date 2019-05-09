5aa707c2ee17d768ef734559139f4f1c7111a364
Julian <julianorchard@hotmail.com>
2019-05-09 19:47:46 +0100

    more general formatting () command

--- frmt.bat
+++ frmt.bat
@@ -1,12 +1,10 @@
 @echo off
 
 setlocal enabledelayedexpansion
-set /p Input=Beginning of files in this folder you want to rename: 
 
-for %%a in (%Input%*.jpg) do (
+for %%a in (*) do (
 
 	set f=%%a
-
 
 REM sets characters to remove from the file names 
 	set f=!f:^(=!
