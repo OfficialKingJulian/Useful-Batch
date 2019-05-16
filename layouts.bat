2705503203444da3db5d65be4cfe3ac6e396dfb1
julianorchard <hi@julians.email>
2019-05-16 16:20:56 +0100

    added layouts, ls and touch

--- layouts.bat
+++ layouts.bat
@@ -1,42 +1,21 @@
 @echo off
 
-REM bad; not worth effort (at the moment) to fix
+set instr=%1
+shift
 
-for %%A in (%*) do (
-	if %%A == -help (
-	  @echo on
-	    echo.
-		echo layouts command help;
-		echo.
-		echo   -brochures 
-		echo                generates folders for brochure design
-		echo.
-		echo   -months 
-		echo                generates folders for months
-	  @echo off
-	) else if %%A == -brochures (
-		mkdir "1. Archive"
-		mkdir "2. InDesign"
-		mkdir "3. Low Quality"
-		mkdir "4. High Quality"
-		mkdir "5. Print Ready"
-	) else if %%A == -months (
-		mkdir "01. Jan"
-		mkdir "02. Feb"
-		mkdir "03. Mar"
-		mkdir "04. Apr"
-		mkdir "05. May"
-		mkdir "06. Jun"
-		mkdir "07. Jul"
-		mkdir "08. Aug"
-		mkdir "09. Sep"
-		mkdir "10. Oct"
-		mkdir "11. Nov"
-		mkdir "12. Dec"
-	) else (
-	  @echo on
-		echo %%A error, unrecognised parameter
-	  @echo off
-	)
+setlocal enabledelayedexpansion
+
+set /A counter=1
+
+REM  the new way
+for /f "tokens=*" %%a in (C:\CMD\layouts\!instr!.txt) do (
 	
+  REM  0 start or no0 
+	if !counter! GEQ 10 (
+		mkdir "!counter!. %%a"
+	) else (
+		mkdir "0!counter!. %%a"
+	)
+	set /A counter+=1
+
 )
