d9e542a455a66c8d097b683a61e184b021961fd9
sujudo <contact@julians.email>
2019-06-25 10:27:29 +0100

    updated alert box slightly

--- screen-reminder.bat
+++ screen-reminder.bat
@@ -1,6 +1,8 @@
 @echo off
 
-msg * "Take a break every hour!"
 
+cscript msgBox.vbs "Take a break every hour!"
+
+REM msg * "Take a break every hour!"
 REM start "" /wait cmd /c "echo Take a break every hour!&echo(&pause"
 
