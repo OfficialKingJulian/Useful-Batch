e98c2fa75b1353ebea4f8482c87027e80e1148c3
OfficialKingJulian <official@kingjulian.xyz>
2020-01-24 12:56:07 +0000

    removing files

--- tools/drink.ahk
+++
@@ -1,5 +0,0 @@
-Sleep, 2000
-Send, *pVyr~eN"~z}@2Bk
-Sleep, 2000
-Send, {Enter}
-Exit
--- tools/drink.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-
-::
-
-ssh drinker@julian.rocks -p 222 | "C:\Users\jorchard\Documents\Applications\AutoHotkeyU64.exe" "C:\CMD\tools\drink.ahk"
-
