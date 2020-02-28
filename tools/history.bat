39aa807ef879e07fae6f96899a23954c7bc5a33a
OfficialKingJulian <official@kingjulian.xyz>
2020-02-28 11:35:58 +0000

    adding search to history for DC :)

--- tools/history.bat
+++ tools/history.bat
@@ -4,9 +4,16 @@
 :: a certain date... echo the full path
 
 set dat=%1
+set sea=%2
+shift
 shift
 if [%dat%] == [] (
-  echo Enter a date, like -dd/MM/yyyy to get all file before, or +dd/MM/yyyy to get all files after 
+  echo Enter a date, like -dd/MM/yyyy to get all file before, or +dd/MM/yyyy to get all files after.
+  echo You can also enter a search path. 
 ) else (
-  forfiles /S /D %dat% /C "cmd /c echo @path"
+  if [%sea%] == [] (
+    forfiles /S /D %dat% /C "cmd /c echo @path"
+  ) else (
+    forfiles /S /D %dat% /m %sea% /C "cmd /c echo @path"
+  )
 )
